18840a8073a8e513cfa206e39c9a19b62896fbb8
Liuting Du <kangtadlt@gmail.com>
2015-06-30 15:12:12 +0100

    Fix incorrect shortcut icon, closes #60

--- evercamPlay/src/main/java/io/evercam/androidapp/utils/EvercamFile.java
+++ evercamPlay/src/main/java/io/evercam/androidapp/utils/EvercamFile.java
@@ -11,15 +11,8 @@ import io.evercam.androidapp.ParentActivity;
 
 public class EvercamFile
 {
-    private static final String TAG = "evercamplay-EvercamFile";
+    private static final String TAG = "EvercamFile";
     public static final String SUFFIX_JPG = ".jpg";
-
-    public static File getCacheFileAbsolute(Context context, String cameraId)
-    {
-        String cachePath = context.getCacheDir().getAbsolutePath() + File.separator + cameraId +
-                SUFFIX_JPG;
-        return new File(cachePath);
-    }
 
     public static File getCacheFileRelative(Context context, String cameraId)
     {
@@ -34,29 +27,5 @@ public class EvercamFile
                 SUFFIX_JPG;
         externalFile = new File(extCachePath);
         return externalFile;
-    }
-
-    public static Bitmap loadBitmapForCamera(Context context, String cameraId)
-    {
-        try
-        {
-
-            File cacheFile = EvercamFile.getCacheFileRelative(context, cameraId);
-            if(cacheFile.exists())
-            {
-                return BitmapFactory.decodeFile(cacheFile.getPath());
-            }
-        }
-        catch(OutOfMemoryError e)
-        {
-            Log.e(TAG, e.toString() + "-::OOM::-" + Log.getStackTraceString(e));
-        }
-        catch(Exception e)
-        {
-            Log.e(TAG, e.toString() + "::" + Log.getStackTraceString(e));
-
-            ParentActivity.sendToMint(e);
-        }
-        return null;
     }
 }
