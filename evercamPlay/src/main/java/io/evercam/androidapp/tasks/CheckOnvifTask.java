30458cfc0297a3d007014143d3761e094d243357
Liuting Du <kangtadlt@gmail.com>
2015-09-22 17:18:16 +0100

    Improve PTZ

--- evercamPlay/src/main/java/io/evercam/androidapp/tasks/CheckOnvifTask.java
+++ evercamPlay/src/main/java/io/evercam/androidapp/tasks/CheckOnvifTask.java
@@ -3,14 +3,11 @@ package io.evercam.androidapp.tasks;
 import android.os.AsyncTask;
 
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
 import java.util.Locale;
 
 import io.evercam.Camera;
 import io.evercam.EvercamException;
 import io.evercam.Model;
-import io.evercam.PTZException;
-import io.evercam.PTZPreset;
 import io.evercam.androidapp.dto.EvercamCamera;
 import io.evercam.androidapp.video.VideoActivity;
 
@@ -18,11 +15,13 @@ public class CheckOnvifTask extends AsyncTask<Void, Void, Boolean>
 {
     private final String TAG = "CheckOnvifTask";
     private WeakReference<VideoActivity> videoActivityWeakReference;
+    private EvercamCamera mEvercamCamera;
     private String modelId;
     private String cameraId;
 
     public CheckOnvifTask(VideoActivity videoActivity, EvercamCamera camera)
     {
+        this.mEvercamCamera = camera;
         videoActivityWeakReference = new WeakReference<>(videoActivity);
         this.modelId = camera.getModel().toLowerCase(Locale.UK);
         this.cameraId = camera.getCameraId();
@@ -45,25 +44,11 @@ public class CheckOnvifTask extends AsyncTask<Void, Void, Boolean>
                 Camera camera = Camera.getById(cameraId, false);
                 if(camera.getRights().isFullRight())
                 {
-                    ArrayList<PTZPreset> allPresets = PTZPreset.getAllPresets(cameraId);
-                    getVideoActivity().presetList = allPresets;
-
-                    ArrayList<PTZPreset> customPresets = removeSystemPresetsFrom(allPresets);
-
-                    if(customPresets.size() > 0)
-                    {
-                        getVideoActivity().presetList = customPresets;
-                    }
-
                     return true;
                 }
             }
         }
         catch(EvercamException e)
-        {
-            e.printStackTrace();
-        }
-        catch(PTZException e)
         {
             e.printStackTrace();
         }
@@ -77,29 +62,14 @@ public class CheckOnvifTask extends AsyncTask<Void, Void, Boolean>
         {
             getVideoActivity().isPtz = hasPtz;
             getVideoActivity().showPtzControl(hasPtz);
+
+            new LoadPresetsTask(getVideoActivity(), mEvercamCamera)
+                    .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         }
     }
 
     private VideoActivity getVideoActivity()
     {
         return videoActivityWeakReference.get();
-    }
-
-    private ArrayList<PTZPreset> removeSystemPresetsFrom(ArrayList<PTZPreset> allPresets)
-    {
-        ArrayList<PTZPreset> customPresets = new ArrayList<>();
-        if(allPresets.size() > 0)
-        {
-            //Exclude presets with token >= 33 and only keep those user defined presets
-            for(PTZPreset preset : allPresets)
-            {
-                int tokenInt = Integer.valueOf(preset.getToken());
-                if(tokenInt < 33)
-                {
-                    customPresets.add(preset);
-                }
-            }
-        }
-        return customPresets;
     }
 }
